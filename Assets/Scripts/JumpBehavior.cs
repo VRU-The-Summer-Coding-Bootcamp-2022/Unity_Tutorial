9d5b4a96edf76e8a3ffa1713ec34825f0b17727c
Mohammad Mehdi Afkhami <m.m.afkhami@outlook.com>
2022-08-05 04:03:01 +0430

    jump improved  and a bug fixed jump count whould be 0 after hit the ground

--- Assets/Scripts/JumpBehavior.cs
+++ Assets/Scripts/JumpBehavior.cs
@@ -4,66 +4,76 @@ using UnityEngine;
 
 public class JumpBehavior : MonoBehaviour
 {
-    [SerializeField] float jumpSpeed;
-    [Range(0,1)]
-    [SerializeField] float fallSpeed;
+    [SerializeField] float jumpSpeed = 16;
+    [SerializeField, Range(0f, 1f)] float fallSpeed = 1;
     [SerializeField] Vector3 groundCheckOffcet;
     [SerializeField] Vector3 groundCheckSize;
     [SerializeField] LayerMask groundCheckMask;
-
-    [SerializeField] int jumpCount=2;
-    private int currentjumpCount=2;
-    [SerializeField] float earlyTime=1;
-    private float currentEarlyTime = 0;
-
+    [SerializeField] float earlyJump = 0.5f;
+    [SerializeField] int jumpCount=1;
+    
     private Rigidbody rb;
-    private bool _grounded = true;
-    private bool leftGround = false;
-    private bool touchedGround = false;
-    private bool hasJumped = false;
+    private float currentEarlyJump;
+    private Vector3 jumpPosTest = Vector3.negativeInfinity;
+    private bool _grounded;
+    private int currentJumpCounter = 0;
+    private bool touchedGround;
+
     private void Start() => rb = GetComponent<Rigidbody>();
 
-    private void FixedUpdate(){
+    private void FixedUpdate() => SetGroundCeckFlags();
+
+    private void SetGroundCeckFlags()
+    {
         bool currentGroundCheck = IsGrounded();
-        if(_grounded != currentGroundCheck)
-        {
-            touchedGround =  currentGroundCheck? true:false;
-            leftGround = currentGroundCheck? false:true;
-        }
-        _grounded = currentGroundCheck;
-
-
+        if (currentGroundCheck != _grounded) //if ground check status is diffrent
+            touchedGround = currentGroundCheck ? true : false;
+        _grounded = IsGrounded();
     }
+
     private void Update()
     {
-        if (touchedGround){
-            currentjumpCount=jumpCount;
+        if (touchedGround)
+        {
+            currentJumpCounter = touchedGround ? jumpCount : currentJumpCounter;
             touchedGround = false;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space))
-            currentEarlyTime = earlyTime;
-        else
-            currentEarlyTime -= Time.deltaTime;
+        // since this might glitch we set the counter on 0 y velocity 
+        else if (Mathf.Abs(rb.velocity.y) <= 0.01f && currentJumpCounter == 0)
+            currentJumpCounter = _grounded ? jumpCount : currentJumpCounter;
         
-        if (currentjumpCount>0 && currentEarlyTime>0 )
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            currentjumpCount--;
-            currentEarlyTime=0;
+            currentEarlyJump = earlyJump;
+            jumpPosTest = transform.position;
+        }
+        else
+            currentEarlyJump -= Time.deltaTime;
+
+        if (currentJumpCounter>0 && currentEarlyJump> 0 )
+        {
+            currentJumpCounter--;
+            currentEarlyJump = 0;
             Jump();
         }
         else if (rb.velocity.y>0 && Input.GetKeyUp(KeyCode.Space))
         {
-            EarlyFall();
+            Fall();
         }
-
     }
+
+    private void Fall() => rb.velocity += Vector3.down * rb.velocity.y * fallSpeed;
+
+    private void Jump() => rb.velocity += Vector3.up * jumpSpeed;
+
     private bool IsGrounded() => Physics.CheckBox(transform.position + groundCheckOffcet, groundCheckSize, Quaternion.identity, groundCheckMask);
-    private void Jump()=> rb.velocity += Vector3.up * jumpSpeed;
-    private void EarlyFall() => rb.velocity += Vector3.down*rb.velocity.y * fallSpeed;
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = IsGrounded() ? Color.red : Color.green;
         Gizmos.DrawWireCube(transform.position + groundCheckOffcet, groundCheckSize*2);
+
+        //Gizmos.color = Color.yellow;
+        //Gizmos.DrawWireCube(jumpPosTest + groundCheckOffcet, groundCheckSize * 2);
     }
 }
